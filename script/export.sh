895279ed03fd122779102bf4b259f48232dae7c6
Your Name <you@example.com>
2018-04-01 21:23:21 +0700

    ok

--- script/export.sh
+++ script/export.sh
@@ -1,4 +1,4 @@
 python pix2pix/pix2pix_128x1024.py  \
     --mode export \
-    --checkpoint pix2pix/output/128x1024_64_v2 \
-    --output_dir pix2pix/output/frozen/128x1024_v2
+    --checkpoint pix2pix/output/128x1024_64_v3\
+    --output_dir pix2pix/output/frozen/128x1024_v3
